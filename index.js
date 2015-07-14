3a4abea1f47194f8277badbb624751db95e501dc
Matthew Bauer <mbauer@lexmark.com>
2015-07-14 17:19:37 -0500

    Use global module.

---
+++ index.js
@@ -0,0 +1,3 @@
+require('./core.js')
+require('./retro.js')
+module.exports = Module
---
+++ retro.js
@@ -0,0 +1,617 @@
+/* global Module */
+
+/* Wrapper around core Modules. */
+
+// TODO: should avoid hardcoding type sizes
+// TODO: should figure out a good way to organize constants
+//  (grouping as objects might make things more complicated)
+// TODO: Make sure this is compiled
+
+Module.LANGUAGE_CHINESE_SIMPLIFIED = 11
+Module.LANGUAGE_CHINESE_TRADITIONAL = 10
+Module.LANGUAGE_KOREAN = 9
+Module.LANGUAGE_RUSSIAN = 8
+Module.LANGUAGE_PORTUGUESE = 7
+Module.LANGUAGE_DUTCH = 6
+Module.LANGUAGE_ITALIAN = 5
+Module.LANGUAGE_GERMAN = 4
+Module.LANGUAGE_SPANISH = 3
+Module.LANGUAGE_FRENCH = 2
+Module.LANGUAGE_JAPANESE = 1
+Module.LANGUAGE_ENGLISH = 0
+Module.K_UNDO = 322
+Module.K_EURO = 321
+Module.K_POWER = 320
+Module.K_MENU = 319
+Module.K_BREAK = 318
+Module.K_SYSREQ = 317
+Module.K_PRINT = 316
+Module.K_HELP = 315
+Module.K_COMPOSE = 314
+Module.K_MODE = 313
+Module.K_RSUPER = 312
+Module.K_LSUPER = 311
+Module.K_LMETA = 310
+Module.K_RMETA = 309
+Module.K_LALT = 308
+Module.K_RALT = 307
+Module.K_LCTRL = 306
+Module.K_RCTRL = 305
+Module.K_LSHIFT = 304
+Module.K_RSHIFT = 303
+Module.K_SCROLLOCK = 302
+Module.K_CAPSLOCK = 301
+Module.K_NUMLOCK = 300
+Module.K_F15 = 296
+Module.K_F14 = 295
+Module.K_F13 = 294
+Module.K_F12 = 293
+Module.K_F11 = 292
+Module.K_F10 = 291
+Module.K_F9 = 290
+Module.K_F8 = 289
+Module.K_F7 = 288
+Module.K_F6 = 287
+Module.K_F5 = 286
+Module.K_F4 = 285
+Module.K_F3 = 284
+Module.K_F2 = 283
+Module.K_F1 = 282
+Module.K_PAGEDOWN = 281
+Module.K_PAGEUP = 280
+Module.K_END = 279
+Module.K_HOME = 278
+Module.K_INSERT = 277
+Module.K_LEFT = 276
+Module.K_RIGHT = 275
+Module.K_DOWN = 274
+Module.K_UP = 273
+Module.K_KP_EQUALS = 272
+Module.K_KP_ENTER = 271
+Module.K_KP_PLUS = 270
+Module.K_KP_MINUS = 269
+Module.K_KP_MULTIPLY = 268
+Module.K_KP_DIVIDE = 267
+Module.K_KP_PERIOD = 266
+Module.K_KP9 = 265
+Module.K_KP8 = 264
+Module.K_KP7 = 263
+Module.K_KP6 = 262
+Module.K_KP5 = 261
+Module.K_KP4 = 260
+Module.K_KP3 = 259
+Module.K_KP2 = 258
+Module.K_KP1 = 257
+Module.K_KP0 = 256
+Module.K_DELETE = 127
+Module.K_z = 122
+Module.K_y = 121
+Module.K_x = 120
+Module.K_w = 119
+Module.K_v = 118
+Module.K_u = 117
+Module.K_t = 116
+Module.K_s = 115
+Module.K_r = 114
+Module.K_q = 113
+Module.K_p = 112
+Module.K_o = 111
+Module.K_n = 110
+Module.K_m = 109
+Module.K_l = 108
+Module.K_k = 107
+Module.K_j = 106
+Module.K_i = 105
+Module.K_h = 104
+Module.K_g = 103
+Module.K_f = 102
+Module.K_e = 101
+Module.K_d = 100
+Module.K_c = 99
+Module.K_b = 98
+Module.K_a = 97
+Module.K_BACKQUOTE = 96
+Module.K_UNDERSCORE = 95
+Module.K_CARET = 94
+Module.K_RIGHTBRACKET = 93
+Module.K_BACKSLASH = 92
+Module.K_LEFTBRACKET = 91
+Module.K_AT = 64
+Module.K_QUESTION = 63
+Module.K_GREATER = 62
+Module.K_EQUALS = 61
+Module.K_LESS = 60
+Module.K_SEMICOLON = 59
+Module.K_COLON = 58
+Module.K_9 = 57
+Module.K_8 = 56
+Module.K_7 = 55
+Module.K_6 = 54
+Module.K_5 = 53
+Module.K_4 = 52
+Module.K_3 = 51
+Module.K_2 = 50
+Module.K_1 = 49
+Module.K_0 = 48
+Module.K_SLASH = 47
+Module.K_PERIOD = 46
+Module.K_MINUS = 45
+Module.K_COMMA = 44
+Module.K_PLUS = 43
+Module.K_ASTERISK = 42
+Module.K_RIGHTPAREN = 41
+Module.K_LEFTPAREN = 40
+Module.K_QUOTE = 39
+Module.K_AMPERSAND = 38
+Module.K_DOLLAR = 36
+Module.K_HASH = 35
+Module.K_QUOTEDBL = 34
+Module.K_EXCLAIM = 33
+Module.K_SPACE = 32
+Module.K_ESCAPE = 27
+Module.K_PAUSE = 19
+Module.K_RETURN = 13
+Module.K_CLEAR = 12
+Module.K_TAB = 9
+Module.K_BACKSPACE = 8
+Module.K_UNKNOWN = 0
+Module.K_FIRST = 0
+Module.KMOD_SCROLLOCK = 64
+Module.KMOD_CAPSLOCK = 32
+Module.KMOD_NUMLOCK = 16
+Module.KMOD_META = 8
+Module.KMOD_ALT = 4
+Module.KMOD_CTRL = 2
+Module.KMOD_SHIFT = 1
+Module.KMOD_NONE = 0
+Module.LOG_DEBUG = 0
+Module.LOG_INFO = 1
+Module.LOG_WARN = 2
+Module.LOG_ERROR = 3
+Module.SENSOR_ACCELEROMETER_ENABLE = 0
+Module.SENSOR_ACCELEROMETER_DISABLE = 1
+Module.CAMERA_BUFFER_OPENGL_TEXTURE = 0
+Module.CAMERA_BUFFER_RAW_FRAMEBUFFER = 1
+Module.RUMBLE_STRONG = 0
+Module.RUMBLE_WEAK = 1
+Module.HW_CONTEXT_OPENGLES_VERSION = 5
+Module.HW_CONTEXT_OPENGLES3 = 4
+Module.HW_CONTEXT_OPENGL_CORE = 3
+Module.HW_CONTEXT_OPENGLES2 = 2
+Module.HW_CONTEXT_OPENGL = 1
+Module.HW_CONTEXT_NONE = 0
+Module.PIXEL_FORMAT_RGB565 = 2
+Module.PIXEL_FORMAT_XRGB8888 = 1
+Module.PIXEL_FORMAT_RGB565 = 0
+Module.API_VERSION = 1
+Module.DEVICE_TYPE_SHIFT = 8
+Module.DEVICE_NONE = 0
+Module.DEVICE_JOYPAD = 1
+Module.DEVICE_MOUSE = 2
+Module.DEVICE_KEYBOARD = 3
+Module.DEVICE_LIGHTGUN = 4
+Module.DEVICE_ANALOG = 5
+Module.DEVICE_POINTER = 6
+Module.DEVICE_ID_JOYPAD_B = 0
+Module.DEVICE_ID_JOYPAD_Y = 1
+Module.DEVICE_ID_JOYPAD_SELECT = 2
+Module.DEVICE_ID_JOYPAD_START = 3
+Module.DEVICE_ID_JOYPAD_UP = 4
+Module.DEVICE_ID_JOYPAD_DOWN = 5
+Module.DEVICE_ID_JOYPAD_LEFT = 6
+Module.DEVICE_ID_JOYPAD_RIGHT = 7
+Module.DEVICE_ID_JOYPAD_A = 8
+Module.DEVICE_ID_JOYPAD_X = 9
+Module.DEVICE_ID_JOYPAD_L = 10
+Module.DEVICE_ID_JOYPAD_R = 11
+Module.DEVICE_ID_JOYPAD_L2 = 12
+Module.DEVICE_ID_JOYPAD_R2 = 13
+Module.DEVICE_ID_JOYPAD_L3 = 14
+Module.DEVICE_ID_JOYPAD_R3 = 15
+Module.DEVICE_INDEX_ANALOG_LEFT = 0
+Module.DEVICE_INDEX_ANALOG_RIGHT = 1
+Module.DEVICE_ID_ANALOG_X = 0
+Module.DEVICE_ID_ANALOG_Y = 1
+Module.DEVICE_ID_MOUSE_X = 0
+Module.DEVICE_ID_MOUSE_Y = 1
+Module.DEVICE_ID_MOUSE_LEFT = 2
+Module.DEVICE_ID_MOUSE_RIGHT = 3
+Module.DEVICE_ID_MOUSE_WHEELUP = 4
+Module.DEVICE_ID_MOUSE_WHEELDOWN = 5
+Module.DEVICE_ID_MOUSE_MIDDLE = 6
+Module.DEVICE_ID_MOUSE_HORIZ_WHEELUP = 7
+Module.DEVICE_ID_MOUSE_HORIZ_WHEELDOWN = 8
+Module.DEVICE_ID_LIGHTGUN_X = 0
+Module.DEVICE_ID_LIGHTGUN_Y = 1
+Module.DEVICE_ID_LIGHTGUN_TRIGGER = 2
+Module.DEVICE_ID_LIGHTGUN_CURSOR = 3
+Module.DEVICE_ID_LIGHTGUN_TURBO = 4
+Module.DEVICE_ID_LIGHTGUN_PAUSE = 5
+Module.DEVICE_ID_LIGHTGUN_START = 6
+Module.DEVICE_ID_POINTER_X = 0
+Module.DEVICE_ID_POINTER_Y = 1
+Module.DEVICE_ID_POINTER_PRESSED = 2
+Module.REGION_NTSC = 0
+Module.REGION_PAL = 1
+Module.MEMORY_SAVE_RAM = 0
+Module.MEMORY_RTC = 1
+Module.MEMORY_SYSTEM_RAM = 2
+Module.MEMORY_VIDEO_RAM = 3
+Module.ENVIRONMENT_SET_ROTATION = 1
+Module.ENVIRONMENT_GET_OVERSCAN = 2
+Module.ENVIRONMENT_GET_CAN_DUPE = 3
+Module.ENVIRONMENT_SET_MESSAGE = 6
+Module.ENVIRONMENT_SHUTDOWN = 7
+Module.ENVIRONMENT_SET_PERFORMANCE_LEVEL = 8
+Module.ENVIRONMENT_GET_SYSTEM_DIRECTORY = 9
+Module.ENVIRONMENT_SET_PIXEL_FORMAT = 10
+Module.ENVIRONMENT_SET_INPUT_DESCRIPTORS = 11
+Module.ENVIRONMENT_SET_KEYBOARD_CALLBACK = 12
+Module.ENVIRONMENT_SET_DISK_CONTROL_INTERFACE = 13
+Module.ENVIRONMENT_SET_HW_RENDER = 14
+Module.ENVIRONMENT_GET_VARIABLE = 15
+Module.ENVIRONMENT_SET_VARIABLES = 16
+Module.ENVIRONMENT_GET_VARIABLE_UPDATE = 17
+Module.ENVIRONMENT_SET_SUPPORT_NO_GAME = 18
+Module.ENVIRONMENT_GET_LIBRETRO_PATH = 19
+Module.ENVIRONMENT_SET_AUDIO_CALLBACK = 22
+Module.ENVIRONMENT_SET_FRAME_TIME_CALLBACK = 21
+Module.ENVIRONMENT_GET_RUMBLE_INTERFACE = 23
+Module.ENVIRONMENT_GET_INPUT_DEVICE_CAPABILITIES = 24
+Module.ENVIRONMENT_GET_LOG_INTERFACE = 27
+Module.ENVIRONMENT_GET_PERF_INTERFACE = 28
+Module.ENVIRONMENT_GET_LOCATION_INTERFACE = 29
+Module.ENVIRONMENT_GET_CONTENT_DIRECTORY = 30
+Module.ENVIRONMENT_GET_CORE_ASSETS_DIRECTORY = 30
+Module.ENVIRONMENT_GET_SAVE_DIRECTORY = 31
+Module.ENVIRONMENT_SET_SYSTEM_AV_INFO = 32
+Module.ENVIRONMENT_SET_PROC_ADDRESS_CALLBACK = 33
+Module.ENVIRONMENT_SET_SUBSYSTEM_INFO = 34
+Module.ENVIRONMENT_SET_CONTROLLER_INFO = 35
+Module.ENVIRONMENT_SET_GEOMETRY = 37
+Module.ENVIRONMENT_GET_USERNAME = 38
+Module.ENVIRONMENT_GET_LANGUAGE = 39
+Module.MEMDESC_CONST = 1
+Module.MEMDESC_BIGENDIAN = 2
+Module.MEMDESC_ALIGN_2 = 65536
+Module.MEMDESC_ALIGN_4 = 131072
+Module.MEMDESC_ALIGN_8 = 196608
+Module.MEMDESC_MINSIZE_2 = 16777216
+Module.MEMDESC_MINSIZE_4 = 33554432
+Module.MEMDESC_MINSIZE_8 = 50331648
+Module.SIMD_SSE = 1
+Module.SIMD_SSE2 = 2
+Module.SIMD_VMX = 4
+Module.SIMD_VMX128 = 8
+Module.SIMD_AVX = 16
+Module.SIMD_NEON = 32
+Module.SIMD_SSE3 = 64
+Module.SIMD_SSSE3 = 128
+Module.SIMD_MMX = 256
+Module.SIMD_MMXEXT = 512
+Module.SIMD_SSE4 = 1024
+Module.SIMD_SSE42 = 2048
+Module.SIMD_AVX2 = 4096
+Module.SIMD_VFPU = 8192
+Module.SIMD_PS = 16384
+Module.SIMD_AES = 32768
+Module.SIMD_VFPV3 = 65536
+Module.SIMD_VFPV4 = 131072
+Module.SENSOR_ACCELEROMETER_X = 1
+Module.SENSOR_ACCELEROMETER_Y = 1
+Module.SENSOR_ACCELEROMETER_Z = 2
+
+Module._unstringify = function (ptr, str) {
+  var _str = this._malloc(str.length + 1)
+  this._ptrs.push(_str)
+  this.writeStringToMemory(str, _str)
+  this.setValue(ptr, _str, '*')
+  return ptr
+}
+
+Module._stringify = function (ptr) {
+  return this.Pointer_stringify(this.getValue(ptr, '*'))
+}
+
+Module._get_variable = function (ptr) {
+  return {
+    key: this._stringify(ptr),
+    value: this._stringify(ptr + 4)
+  }
+}
+
+Module._get_av_info = function (_data) {
+  return {
+    geometry: {
+      base_width: this.getValue(_data, 'i32'),
+      base_height: this.getValue(_data + 4, 'i32'),
+      max_width: this.getValue(_data + 8, 'i32'),
+      max_height: this.getValue(_data + 12, 'i32'),
+      aspect_ratio: this.getValue(_data + 16, 'float')
+    },
+    timing: {
+      fps: this.getValue(_data + 24, 'double'),
+      sample_rate: this.getValue(_data + 32, 'double')
+    }
+  }
+}
+
+Module._set_info = function (_info, info) {
+  if (info.path) {
+    this._unstringify(_info, info.path)
+  }
+  if (info.meta) {
+    this._unstringify(_info + 12, info.meta)
+  }
+  var _data = this._malloc(info.data.length)
+  this._ptrs.push(_data)
+  new Uint8Array(this.HEAP8.buffer, _data, info.data.length).set(info.data)
+  this.setValue(_info + 4, _data, '*')
+  this.setValue(_info + 8, info.data.length, 'i32')
+  return _info
+}
+
+Module.get_system_info = function () {
+  var _data = this._malloc(14)
+  this._retro_get_system_info(_data)
+  var obj = {
+    library_name: this._stringify(_data),
+    library_version: this._stringify(_data + 4),
+    valid_extensions: this._stringify(_data + 8),
+    need_fullpath: this.getValue(_data + 12, 'i8') > 0,
+    block_extract: this.getValue(_data + 13, 'i8') > 0
+  }
+  this._free(_data)
+  return obj
+}
+
+Module.get_system_av_info = function () {
+  var _data = this._malloc(40)
+  this._retro_get_system_av_info(_data)
+  var info = this._get_av_info(_data)
+  this._free(_data)
+  return info
+}
+
+Module.serialize = function () {
+  var size = this._retro_serialize_size()
+  var _data = this._malloc(size)
+  var data = false
+  if (this._retro_serialize(_data, size)) {
+    data = new Uint8Array(this.HEAP8.buffer, _data, size)
+  }
+  // this._free(_data)
+  return data
+}
+
+Module.unserialize = function (data) {
+  var _data = this._malloc(data.length)
+  new Uint8Array(this.HEAP8.buffer, _data, data.length).set(data)
+  var result = this._retro_unserialize(_data, data.length)
+  // this._free(_data)
+  return result
+}
+
+Module.cheat_set = function (index, enabled, code) {
+  var _code = this._malloc(code.length)
+  this._ptrs.push(_code)
+  this.writeStringToMemory(code, _code)
+  this._retro_cheat_set(index, enabled, _code)
+}
+
+Module.load_game = function (data) {
+  var _info = this._malloc(16)
+  this._ptrs.push(_info)
+  this._set_info(_info, {data: data})
+  return this._retro_load_game(_info)
+}
+
+Module.load_game_special = function (game_type, infos) {
+  var _info = this._malloc(16 * infos.length)
+  this._ptrs.push(_info)
+  for (var info in infos) {
+    this._set_info(_info + 16 * info, infos[info])
+  }
+  return this._retro_load_game_special(game_type, _info, infos.length)
+}
+
+Module.get_memory_data = function (id) {
+  return new Uint8Array(this.HEAP8.buffer, this._retro_get_memory_data(id), this._retro_get_memory_size(id))
+}
+
+Module.set_environment = function (fn) { // complete libretro spec
+  this._retro_set_environment(Runtime.addFunction(function (fn, cmd, _data) {
+    switch (cmd) {
+      case this.ENVIRONMENT_SHUTDOWN: {
+        return fn(cmd)
+      }
+      case this.ENVIRONMENT_SET_PERFORMANCE_LEVEL:
+      case this.ENVIRONMENT_SET_PIXEL_FORMAT:
+      case this.ENVIRONMENT_SET_ROTATION:
+      case this.ENVIRONMENT_SET_SUPPORT_NO_GAME: {
+        return fn(cmd, this.getValue(_data, 'i32'))
+      }
+      case this.ENVIRONMENT_SET_GEOMETRY: {
+        return fn(cmd, {
+          base_width: this.getValue(_data, 'i32'),
+          base_height: this.getValue(_data + 4, 'i32'),
+          max_width: this.getValue(_data + 8, 'i32'),
+          max_height: this.getValue(_data + 12, 'i32'),
+          aspect_ratio: this.getValue(_data + 16, 'float')
+        })
+      }
+      case this.ENVIRONMENT_SET_INPUT_DESCRIPTORS: {
+        var descriptions = []
+        for (var ptr = _data; this.getValue(ptr + 16, '*'); ptr += 20) {
+          descriptions.push({
+            port: this.getValue(ptr, 'i32'),
+            device: this.getValue(ptr + 4, 'i32'),
+            index: this.getValue(ptr + 8, 'i32'),
+            id: this.getValue(ptr + 12, 'i32'),
+            description: this._stringify(ptr + 16)
+          })
+        }
+        return fn(cmd, descriptions)
+      }
+      case this.ENVIRONMENT_SET_MESSAGE: {
+        return fn(cmd, this._stringify(_data), this.getValue(_data + 4, 'i32'))
+      }
+      case this.ENVIRONMENT_SET_SYSTEM_AV_INFO: {
+        return fn(cmd, this._get_av_info(_data))
+      }
+      case this.ENVIRONMENT_SET_VARIABLES: {
+        var variables = []
+        for (ptr = _data; this.getValue(ptr, '*'); ptr += 8) {
+          variables.push(this._get_variable(ptr))
+        }
+        return fn(cmd, variables)
+      }
+      case this.ENVIRONMENT_SET_AUDIO_CALLBACK: {
+        var audio = fn(cmd)
+        this.setValue(_data, Runtime.addFunction(audio.callback))
+        this.setValue(_data + 4, Runtime.addFunction(audio.set_state))
+        return true
+      }
+      case this.ENVIRONMENT_SET_CONTROLLER_INFO: {
+        // var controllers = []
+        // for (var ptr = _data; this.getValue(ptr, '*') != null; ptr += 8) {
+        //   var _controller = this.getValue(ptr, '*')
+        //   var controller = {}
+        //   var num_types = this.getValue(ptr + 4, 'i32')
+        //   for (var type = 0; type < num_types; type += 1) {
+        //   }
+        //   controllers.push(controller)
+        // }
+        return true
+      }
+      case this.ENVIRONMENT_GET_OVERSCAN:
+      case this.ENVIRONMENT_GET_LANGUAGE:
+      case this.ENVIRONMENT_GET_CAN_DUPE:
+      case this.ENVIRONMENT_GET_INPUT_DEVICE_CAPABILITIES:
+      case this.ENVIRONMENT_GET_VARIABLE: {
+        this.setValue(_data, fn(cmd), 'i32')
+        return true
+      }
+      case this.ENVIRONMENT_GET_SYSTEM_DIRECTORY:
+      case this.ENVIRONMENT_GET_LIBRETRO_PATH:
+      case this.ENVIRONMENT_GET_CORE_ASSETS_DIRECTORY:
+      case this.ENVIRONMENT_GET_SAVE_DIRECTORY:
+      case this.ENVIRONMENT_GET_USERNAME: {
+        this._unstringify(_data, fn(cmd))
+        return true
+      }
+      case this.ENVIRONMENT_GET_VARIABLE: {
+        this._unstringify(fn(cmd, this._get_variable(_data)), _data + 4)
+        return true
+      }
+      case this.ENVIRONMENT_GET_LOG_INTERFACE: {
+        var func = fn(cmd)
+        this.setValue(_data, Runtime.addFunction(function (func, level) {
+          var args = []
+          var varargs = Array.prototype.slice.call(arguments, 3)
+          for (var vararg in varargs) {
+            args.push(this.Pointer_stringify(varargs[vararg]))
+          }
+          fn.apply(null, [cmd, level].concat(args))
+        }.bind(this, func)))
+        return true
+      }
+      case this.ENVIRONMENT_GET_LOCATION_INTERFACE: {
+        var location = fn(cmd)
+        func.setValue(_data, Runtime.addFunction(location.start))
+        this.setValue(_data + 4, Runtime.addFunction(location.stop))
+        this.setValue(_data + 8, Runtime.addFunction(function (location, lat, lon, horiz_accuracy, vert_accuracy) {
+          var position = location.get_position()
+          this.setValue(lat, position.lat, 'double')
+          this.setValue(lon, position.lon, 'double')
+          this.setValue(horiz_accuracy, position.horiz_accuracy, 'double')
+          this.setValue(vert_accuracy, position.vert_accuracy, 'double')
+        }.bind(this, location)))
+        this.setValue(_data + 12, Runtime.addFunction(location.set_interval))
+        this.setValue(_data + 16, Runtime.addFunction(location.initialized))
+        this.setValue(_data + 20, Runtime.addFunction(location.deinitialized))
+        return true
+      }
+      case this.ENVIRONMENT_GET_RUMBLE_INTERFACE: {
+        this.setValue(_data, Runtime.addFunction(fn(cmd)))
+        return true
+      }
+      default: {
+        return fn(cmd, _data)
+      }
+    }
+  }.bind(this, fn)))
+}
+
+Module.set_video_refresh = function (fn) {
+  this._retro_set_video_refresh(Runtime.addFunction(function (fn, _data, width, height, pitch) {
+    var data = new Uint8Array(this.HEAP8.buffer, _data, height * pitch)
+    fn(data, width, height, pitch)
+    this._free(_data)
+  }.bind(this, fn)))
+}
+
+Module.set_audio_sample_batch = function (fn) {
+  this._retro_set_audio_sample_batch(Runtime.addFunction(function (fn, _data, frames) {
+    var left = new Float32Array(frames)
+    var right = new Float32Array(frames)
+    var data = new Int16Array(this.HEAP8.buffer, _data, frames * 4)
+    for (var i = 0; i < frames; i++) {
+      left[i] = data[i * 2] / 0x8000
+      right[i] = data[i * 2 + 1] / 0x8000
+    }
+    this._free(_data)
+    return fn(left, right, frames)
+  }.bind(this, fn)))
+}
+
+Module.set_audio_sample = function (fn) {
+  this._retro_set_audio_sample(Runtime.addFunction(fn))
+}
+
+Module.set_input_poll = function (fn) {
+  this._retro_set_input_poll(Runtime.addFunction(fn))
+}
+
+Module.set_input_state = function (fn) {
+  this._retro_set_input_state(Runtime.addFunction(fn))
+}
+
+Module.init = function () {
+  this._retro_init()
+}
+
+Module.deinit = function () {
+  this._retro_deinit()
+}
+
+Module.api_version = function () {
+  return this._retro_api_version()
+}
+
+Module.reset = function () {
+  this._retro_reset()
+}
+
+Module.run = function () {
+  this._retro_run()
+}
+
+Module.unload_game = function () {
+  this._retro_unload_game()
+  for (var ptr in this._ptrs) {
+    this._free(this._ptrs[ptr])
+  }
+  this._ptrs = []
+}
+
+Module.get_region = function () {
+  return this._retro_get_region()
+}
+Module.cheat_reset = function () {
+  this._retro_cheat_reset()
+}
+
+Module.set_controller_port_device = function (port, device) {
+  this._retro_set_controller_port_device(port, device)
+}
