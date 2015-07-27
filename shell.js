ae7d58798c7b26f3eee1d0c3d9e93d164bc7f4d5
Matthew Justin Bauer <mjbauer95@gmail.com>
2015-07-27 18:03:49 -0500

    Get rid of variables in shell.

--- shell.js
+++ shell.js
@@ -3,8 +3,6 @@ var ENVIRONMENT_IS_WEB = true;
 var ENVIRONMENT_IS_NODE = false;
 var ENVIRONMENT_IS_SHELL = false;
 var Module = {
-  'noInitialRun': true,
-  'noExitRuntime': true,
   'print': function () {},
   'printErr': function () {}
 };
