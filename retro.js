7a4567d4f016733a9da0ae224c6972fc2ed602fa
Matthew Bauer <mjbauer95@gmail.com>
2015-09-25 01:00:51 -0500

    Remove amdefine refs.

--- retro.js
+++ retro.js
@@ -1,9 +1,4 @@
 'format amd'
-if (typeof define !== 'function') {
-  if (typeof require === 'function') {
-    var define = require('amdefine')(module)
-  }
-}
 define(['./core'], function (Core) {
   function addHelpers () {
     this.LANGUAGE_ENGLISH = 0
