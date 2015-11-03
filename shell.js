96f2e14bcc80f5470944c360832666f2618fa5f4
Matthew Bauer <mjbauer95@gmail.com>
2015-11-03 10:38:57 -0600

    Switch to cjs.

--- shell.js
+++ shell.js
@@ -1,14 +1,13 @@
-'format amd'
-define([], function () {
-  var ENVIRONMENT_IS_WEB = true
-  var ENVIRONMENT_IS_NODE = false
-  var ENVIRONMENT_IS_SHELL = false
-  var Module = {
-    'print': function () {},
-    'printErr': function () {},
-    'noExitRuntime': true,
-    'noInitialRun': true
-  }
-  {{BODY}}
-  return Module
-})
+'format cjs'
+var ENVIRONMENT_IS_WEB = true
+var ENVIRONMENT_IS_NODE = false
+var ENVIRONMENT_IS_SHELL = false
+var Module = {
+  'print': function () {},
+  'printErr': function () {},
+  'noExitRuntime': true,
+  'noInitialRun': true
+}
+{{BODY}}
+module.exports  = Module
+
